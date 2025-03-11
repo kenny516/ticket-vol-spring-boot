d75e540e1f13b024d321cf544a2d94b99362b2b6
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-11 22:06:28 +0300

    add some style and design

--- src/main/java/com/ticketvol/ticketVol/config/AuthIntercepter.java
+++ src/main/java/com/ticketvol/ticketVol/config/AuthIntercepter.java
@@ -1,5 +1,6 @@
 package com.ticketvol.ticketVol.config;
 
+import com.ticketvol.ticketVol.model.Utilisateur;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.stereotype.Component;
@@ -10,7 +11,7 @@ public class AuthIntercepter implements HandlerInterceptor {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
-        String utilisateur = (String) request.getSession().getAttribute("utilisateur");
+        Utilisateur utilisateur = (Utilisateur) request.getSession().getAttribute("utilisateur");
         if (utilisateur == null) {
             response.sendRedirect("/login");
             return false;
