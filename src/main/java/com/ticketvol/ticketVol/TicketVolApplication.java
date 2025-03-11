5dd2e6530dac6ddbc0c5143fc48ac1eb6f6c8d2d
CHAN Kenny <126701465+kenny516@users.noreply.github.com>
2025-03-11 19:08:04 +0300

    init spring boot project

---
+++ src/main/java/com/ticketvol/ticketVol/TicketVolApplication.java
@@ -0,0 +1,13 @@
+package com.ticketvol.ticketVol;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketVolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketVolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ticketvol/ticketVol/TicketVolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ticketvol.ticketVol;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketVolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
